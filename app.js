a4ba1dd67f7ce278740fb25c9d45fe0e9906064c
DANIEL_GRADY <anakria977@gmail.com>
2023-12-08 15:55:46 -0500

    Completed part 3. Added edge cases function and guess count.

--- app.js
+++ app.js
@@ -1,34 +1,49 @@
+// Minimum number of guesses.
 const minGuess = 1;
+
+//Maximum number of guesses.
 const maxGuess = 10;
+
+// Assign random number to secret number.
 const secretNumber = getRandomIntInclusive(minGuess, maxGuess);
-var userInput = prompt("Please guess the secret number between " + minGuess + " and " + maxGuess + ".");
 
-
+// Function uses minGuess and maxGuess to calculate random number.
 function getRandomIntInclusive(min, max) {
-    return Math.floor(
-        Math.random() * (Math.floor(max) - Math.ceil(min) + 1) + min
-    );
+    return Math.floor(Math.random() * (Math.floor(max) - Math.ceil(min) + 1) + min);
 }
-function guessOnce(input) {
-    let guessedCorrectly = false;
-    input = parseInt(input);
 
-    while (!guessedCorrectly) {
+// Function to create prompt for user.
+function userPrompt() {
+    let answer = Number(prompt("Please guess the secret number between " + minGuess + " and " + maxGuess + "."));
+    return answer;
+}
+function edgeCases(userNumber) {
+    if (isNaN(userNumber) || userNumber === null || !Number.isInteger(userNumber)) {
+        alert("Please enter a valid input");
+        return false;
+    }
+    return true;
+}
+// Main function to calculate the guessing game.
+function guessGame() {
+    // Number of current user guesses.
+    let guesses = 0;
+    // While loop breaks when return is called.
+    while (true) {
+        //Prompt the user for a guess and parse into a number.
+        let userInput = userPrompt();
+        // Check for edge cases
+        if (!edgeCases(userInput)) {
+            continue; // Skip the loop and prompt again, otherwise continue.
+        }
+        // Switch case to determine if userInput === secretNumber. If input !== secretNumber, increment guesses and move to next case.
         switch (true) {
-            case input < secretNumber:
-                alert("Higher");
-                break;
-            case input > secretNumber:
-                alert("Lower");
-                break;
-            case input === secretNumber:
-                alert("Correct");
-                guessedCorrectly = true;
-                break;
-            default:
-                alert("Invalid input");
-                input = prompt("Please guess the secret number");
+            case userInput < secretNumber: alert('Higher'); guesses++; break;
+            case userInput > secretNumber: alert('Lower'); guesses++; break;
+            case userInput === secretNumber: guesses++; alert('Correct! You guessed ' + guesses + ' times.'); return; // Breaks while loop.
+            default: alert("Invalid input"); break;
         }
     }
 }
-guessOnce(userInput);
+// Invoke main function.
+guessGame();
