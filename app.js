a4cc68f99c7933548045a4d9ab996cf376e99784
DANIEL_GRADY <anakria977@gmail.com>
2023-12-08 13:16:39 -0500

    New update before consilidating functions

--- app.js
+++ app.js
@@ -1,34 +1,25 @@
 const minGuess = 1;
 const maxGuess = 10;
 const secretNumber = getRandomIntInclusive(minGuess, maxGuess);
+var userInput = prompt("Please guess the secret number between " + minGuess + " and " + maxGuess + ".");
+
 
 function getRandomIntInclusive(min, max) {
     return Math.floor(
         Math.random() * (Math.floor(max) - Math.ceil(min) + 1) + min
     );
 }
-var userInput = prompt(
-    "Please guess the secret number between " +
-        minGuess +
-        " and " +
-        maxGuess +
-        "."
-);
-
 function guessOnce(input) {
     let guessedCorrectly = false;
+    input = parseInt(input);
 
     while (!guessedCorrectly) {
-        input = parseInt(input);
-
         switch (true) {
             case input < secretNumber:
                 alert("Higher");
-                input = prompt("Please guess the secret number");
                 break;
             case input > secretNumber:
                 alert("Lower");
-                input = prompt("Please guess the secret number");
                 break;
             case input === secretNumber:
                 alert("Correct");
